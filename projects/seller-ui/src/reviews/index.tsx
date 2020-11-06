58a2870292ff30dc3b11640a729f677ac4e7a3b8
U K ANIRUDH <anirudh.anirudh93@gmail.com>
2020-11-06 21:05:56 +0530

    Anirudh: review average

--- projects/seller-ui/src/reviews/index.tsx
+++ projects/seller-ui/src/reviews/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from 'antd';
+import { Typography, Divider, Rate } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../store/rootReducer';
 import Review from './review';
@@ -12,6 +12,9 @@ const ReviewsSection = () => {
     return (
         <>
             <Title level={2}>Reviews</Title>
+            <Rate disabled defaultValue={4} />
+            <Title level={4}>Total {reviewsList.length} review</Title>
+            <Divider />
             <Review reviewsList={reviewsList} />
         </>
     )
