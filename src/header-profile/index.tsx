6c524f6e8d02ad44094c2453e6e58f831c0eb4c0
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2020-12-21 17:56:48 +0530

    Adding breadcrumbs.

--- src/header-profile/index.tsx
+++ src/header-profile/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Badge, Card, Dropdown, Menu, Modal, notification, Popconfirm, Tooltip, Typography } from 'antd';
+import { Badge, Breadcrumb, Card, Dropdown, Menu, Modal, notification, Popconfirm, Tooltip, Typography } from 'antd';
 import { BellFilled, ContactsFilled, LogoutOutlined } from '@ant-design/icons';
 import MenuItem from 'antd/lib/menu/MenuItem';
+import { Redirect } from 'react-router-dom';
 
 import { RootState } from '../store/rootReducer';
 import { UserStateModel } from '../store/loginReducer/types';
@@ -17,6 +18,20 @@ const UserHeader = () => {
     const fieldOfficer = "Mx. Field Officer";
     const fieldOfficerNumber = "9876543210";
     const [notificationNumber, setNotificationNumber] = useState(2);
+    const [isLogout, setLogout] = useState(false); 
+    var produce, matches, transaction, feedback;
+
+    if (loginState.is_buyer) {
+        produce = "#" + "buyer-ui-app";
+        matches = "#" + "buyer-ui-matches";
+        transaction = "#" + "buyer-ui-transactions";
+        feedback = "#" + "buyer-ui-feedback";
+    } else {
+        produce = "#" + "seller-ui-app";
+        matches = "#" + "seller-ui-matches";
+        transaction = "#" + "seller-ui-transactions";
+        feedback = "#" + "seller-ui-feedback";
+    }
 
     // clear=true Means Clear one notification. clear=false Means Add one notification
     const clearNotification = (clear: boolean) => {
@@ -48,12 +63,21 @@ const UserHeader = () => {
                 <MenuItem key="fieldOfficerInfo" onClick={displayFieldOfficerInfo} >
                     Field Officer
                 </MenuItem>
-                <MenuItem key="contactUs">
-                    <a href="mailto:support@vikasbandhu.com" >Contact Us</a>
+                <MenuItem key="contactUs" onClick={displayContactInfo}>
+                    Contact Us
                     {/* Alert */}
                 </MenuItem>
             </Menu>
         );
+    };
+
+    const displayContactInfo = () => {
+        Modal.info({
+            title: 'Contact Us',
+            content: (
+                <Title level={4}>support@vikasbandhu.in</Title>
+            )
+        })
     };
 
     const displayFieldOfficerInfo = () => {
@@ -70,42 +94,50 @@ const UserHeader = () => {
 
     const logout = () => {
         // Logout Functionality
+        setLogout(true);
     };
 
     return (
-      <div className='display-flex-row align-center'>
-            <Badge count={notificationNumber}>
-                <Tooltip title="Notifications">
-                    <DefaultBtn 
-                        shape="circle"
-                        size="large"
-                        icon={<BellFilled style={{ fontSize: "large", paddingLeft: "0.3em"}} />}
-                        onClick={openNotification}
-                    />
-                </Tooltip>
-            </Badge>
-            <Dropdown.Button 
-                overlay={showContactInfo}
-                icon={<ContactsFilled style={{ fontSize: "large" }} />}
-                size="large"
-                className="custom-dropdown-button"
-            />
-            <Title level={4} className='margin-unset'>{loginState.name}</Title>
-            {/* <p className='margin-unset'>Seller Id: {loginState.userId}</p> */}
-            <Popconfirm
-                title="Are you sure you want to logout?"
-                okText="Yes"
-                onConfirm={logout}
-                cancelText="No"
-            >
-                <Tooltip title="logout">
-                    <DefaultBtn
-                        icon={<LogoutOutlined style={{ fontSize: "large", paddingLeft: "0.3em"}}/>}
-                        size="large"
-                        shape="circle"
-                    />
-                </Tooltip>
-            </Popconfirm>
+      <div className="display-flex-row align-center">
+        <Breadcrumb separator=" " className="custom-breadcrumb">
+            <Breadcrumb.Item href={produce} >Produce</Breadcrumb.Item>
+            <Breadcrumb.Item href={matches} >Matches</Breadcrumb.Item>
+            <Breadcrumb.Item href={transaction} >Transaction</Breadcrumb.Item>
+            <Breadcrumb.Item href={feedback} >Feedback</Breadcrumb.Item>
+        </Breadcrumb>
+        <Badge count={notificationNumber} className="custom-badge">
+            <Tooltip title="Notifications">
+                <DefaultBtn 
+                    shape="circle"
+                    size="large"
+                    icon={<BellFilled style={{ fontSize: "large", paddingLeft: "0.3em"}} />}
+                    onClick={openNotification}
+                />
+            </Tooltip>
+        </Badge>
+        <Dropdown.Button 
+            overlay={showContactInfo}
+            icon={<ContactsFilled style={{ fontSize: "large" }} />}
+            size="large"
+            className="custom-dropdown-button"
+        />
+        <Title level={4} className='margin-unset' style={{padding: "0.5em"}}>{loginState.name}</Title>
+        {/* <p className='margin-unset'>Seller Id: {loginState.userId}</p> */}
+        <Popconfirm
+            title="Are you sure you want to logout?"
+            okText="Yes"
+            onConfirm={logout}
+            cancelText="No"
+        >
+            <Tooltip title="logout">
+                <DefaultBtn
+                    icon={<LogoutOutlined style={{ fontSize: "large", paddingLeft: "0.3em"}}/>}
+                    size="large"
+                    shape="circle"
+                />
+            </Tooltip>
+        </Popconfirm>
+        {isLogout && <Redirect to="/" />}
       </div>
     );
 };
