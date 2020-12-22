f43af3997ab1dafc4a924cbb913f08e48cfd1c4e
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2020-12-22 17:12:33 +0530

    Adding  fetchAll Produce, Crops & Variety.

--- src/store/buyerReducer/actions.tsx
+++ src/store/buyerReducer/actions.tsx
@@ -1,10 +1,12 @@
-import { addProduce, getAllProduce } from "../api";
+import { addProduce, getAllProduce, getCropCategoryList, getCropList, getSubCategoryList } from "../api";
 import { RootState } from "../rootReducer";
-import { MasterListProduce, ProduceModel } from "./types";
+import { MasterListProduce } from "./types";
 
 export const UPDATE_MASTER_LIST = 'UPDATE_MASTER_LIST';
 export const GET_MASTER_LIST = 'GET_MASTER_LIST';
 export const UPDATE_PRODUCE_LIST = 'UPDATE_PRODUCE_LIST'
+export const UPDATE_CROPS_LIST = 'UPDATE_CROPS_LIST';
+export const UPDATE_VARIETY_LIST = 'UPDATE_VARIETY_LIST';
 
 export const updateMasterlist = (masterlist: Array<MasterListProduce>) => {
     return {
@@ -13,10 +15,24 @@ export const updateMasterlist = (masterlist: Array<MasterListProduce>) => {
     };
 };
 
-export const updateProduceList = (produceList: Array<ProduceModel>) => {
+export const updateProduceList = (produceList: Array<string>) => {
     return {
         type: UPDATE_PRODUCE_LIST,
         payload: produceList
+    }
+}
+
+export const updateCropsList = (cropsList: Array<string>) => {
+    return {
+        type: UPDATE_CROPS_LIST,
+        payload: cropsList
+    }
+}
+
+export const updateVarietyList = (varietyList: Array<string>) => {
+    return {
+        type: UPDATE_VARIETY_LIST,
+        payload: varietyList
     }
 }
 
@@ -41,6 +57,33 @@ export const getProduceList = () => {
         const getProduceListResponse = await getAllProduce(userName);
         const {Items, Count} = getProduceListResponse || {Items: []}
         console.log('getProduceList', Items);
-        dispatch(updateProduceList(Items as Array<ProduceModel>))
+        // dispatch(updateProduceList(Items as Array<ProduceModel>))
+    }
+}
+
+export const fetchAllProduce = () => {
+    return async(dispatch: any, getState: any) => {
+        const allProduceList = await getCropCategoryList();
+        const {categories} = allProduceList || []
+        console.log('registerUserResponse', categories);
+        dispatch(updateProduceList(categories))
+    }
+}
+
+export const fetchAllCrops = (category: string) => {
+    return async(dispatch: any, getState: any) => {
+        const allCropsList = await getCropList(category);
+        const {crops} = allCropsList || []
+        console.log('Crops List', crops);
+        dispatch(updateCropsList(crops));
+    }
+}
+
+export const fetchAllVariety = (crop: string) => {
+    return async(dispatch: any, getState: any) => {
+        const allVarietyList = await getSubCategoryList(crop);
+        const { crops: {Items: variety}} = allVarietyList || {variety: []}
+        console.log('Variety list', variety);
+        dispatch(updateVarietyList(variety));    
     }
 }
