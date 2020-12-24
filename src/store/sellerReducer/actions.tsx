76a3a59973d409cb27c04f353859e99839c35459
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2020-12-24 16:28:52 +0530

    Added fetchAll Produce, Variety and Grade.

--- src/store/sellerReducer/actions.tsx
+++ src/store/sellerReducer/actions.tsx
@@ -1,10 +1,11 @@
 import { CropModel } from "./types";
-import { getCategoryList, getSubCategoryList, createCrop, getAllCrops } from "../api";
+import { getSubCategoryList, createCrop, getAllCrops, getCropCategoryList, getCropList } from "../api";
 import { RootState } from "../rootReducer";
 
 export const UPDATE_CATEGORIES = 'UPDATE_CATEGORIES';
-export const UPDATE_SUB_CATEGORIES = 'UPDATE_SUB_CATEGORIES';
-export const UPDATE_GRADES = 'UPDATE_GRADES';
+export const UPDATE_PRODUCE = 'UPDATE_PRODUCE';
+export const UPDATE_VARIETY = 'UPDATE_VARIETY';
+export const UPDATE_GRADE = 'UPDATE_GRADE';
 export const UPDATE_FORM = 'UPDATE_FORM';
 export const ADD_NEW_CROP = 'ADD_NEW_CROP';
 export const UPDATE_SELLER_CROPS_LIST = 'UPDATE_SELLER_CROPS_LIST';
@@ -23,10 +24,24 @@ export const updateAllCategories = (categories: Array<string>) => {
     }
 }
 
-export const updateSubCategories = (subCategories: Array<any>) => {
+export const updateProduce = (produce: Array<string>) => {
     return {
-        type: UPDATE_SUB_CATEGORIES,
-        payload: subCategories
+        type: UPDATE_PRODUCE,
+        payload: produce
+    }
+}
+
+export const updateVariety = (variety: Array<any>) => {
+    return {
+        type: UPDATE_VARIETY,
+        payload: variety
+    }
+};
+
+export const updateGrade = (grade: Array<any>) => {
+    return {
+        type: UPDATE_GRADE,
+        payload: grade
     }
 }
 
@@ -39,18 +54,33 @@ export const updateSellerCropsList = (cropsList: Array<any>) => {
 
 export const fetchAllCategories = () => {
     return async(dispatch: any, getState: any) => {
-        const allCategoriesList = await getCategoryList();
-        const {crops} = allCategoriesList || []
-        dispatch(updateAllCategories(crops))
+        const allCategoriesList = await getCropCategoryList();
+        const {categories} = allCategoriesList || []
+        dispatch(updateAllCategories(categories))
     }
 }
 
-export const fetchAllSubCategories = (categoryId: string) => {
+export const fetchAllProduce = (category: string) => {
     return async(dispatch: any, getState: any) => {
-        const allCategoriesList = await getSubCategoryList(categoryId);
-        const {crops: {Items}} = allCategoriesList || {Items: []}
-        // const {status = '', message, data} = result
-        dispatch(updateSubCategories(Items))
+        const allCropList = await getCropList(category);
+        const {crops} = allCropList || []
+        dispatch(updateProduce(crops))
+    }
+}
+
+export const fetchAllVariety = (cropName: string) => {
+    return async(dispatch: any, getState: any) => {
+        const allVarietyList = await getSubCategoryList(cropName);
+        const {crops: {Items: variety}} = allVarietyList || { variety: []}
+        dispatch(updateVariety(variety))
+    }
+}
+
+export const fetchAllGrade = (cropName: string) => {
+    return async(dispatch: any, getState: any) => {
+        const allGradeList = await getSubCategoryList(cropName);
+        const {crops: {Items: grade}} = allGradeList || {grade: []}
+        dispatch(updateGrade(grade))
     }
 }
 
