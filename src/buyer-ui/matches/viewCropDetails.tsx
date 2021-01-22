51e8c2d098649d29f2613f4d7749274fe067ad0f
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-01-22 17:05:16 +0530

    Adding TradeSummary.

--- src/buyer-ui/matches/viewCropDetails.tsx
+++ src/buyer-ui/matches/viewCropDetails.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Col, Divider, Modal, Row, Typography } from 'antd';
 
-import { MatchRequirementModel } from '../../store/sellerReducer/types';
+import { MatchRequirementModel } from '../../store/buyerReducer/types';
 import DefaultBtn from '../../app-components/defaultBtn';
+import TradeSumary from './tradeSummary';
 
 const { Title } = Typography;
 
@@ -14,67 +15,11 @@ interface componentProps {
 
 const ViewCropDetails = (props: componentProps) => {
     const { openDetailsModal, setOpenDetailsModal, cropDetails } = props;
-    const { buyerId, cropName, quantityRequired, pricePerQnt, location } = cropDetails;
     const closeModal = () => setOpenDetailsModal(!openDetailsModal);
+    
     return (
         <Modal visible={openDetailsModal} title="Details" onCancel={closeModal} footer={null}>
-            <Title level={4}>Trade summary</Title>
-            <Row>
-                <Col sm={24} md={12}>
-                    Buyer Id
-                </Col>
-                <Col sm={24} md={12}>
-                    : {buyerId}
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={24} md={12}>
-                    Produce
-                </Col>
-                <Col sm={24} md={12}>
-                    : {cropName}
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={24} md={12}>
-                    Quantity
-                </Col>
-                <Col sm={24} md={12}>
-                    : {quantityRequired}
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={24} md={12}>
-                    Price per quintal
-                </Col>
-                <Col sm={24} md={12}>
-                    : {pricePerQnt}
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={24} md={12}>
-                    Total price
-                </Col>
-                <Col sm={24} md={12}>
-                    : {quantityRequired * pricePerQnt}
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={24} md={12}>
-                    Location
-                </Col>
-                <Col sm={24} md={12}>
-                    : {location}
-                </Col>
-            </Row>
-            <Row>
-                <Col sm={24} md={12}>
-                    Tentative pickup
-                </Col>
-                <Col sm={24} md={12}>
-                    : {'12/08/2020 to 15/08/2020'}
-                </Col>
-            </Row>
+            <TradeSumary cropDetails={cropDetails} />
             <Divider />
             <Row justify="center">
                 <Col>
