d067d26a1f40d2900e74769ba20a822b85a8773d
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-01-27 11:14:12 +0530

    Using toLocaleDateString and toLocaleTimeString

--- src/app-components/utils.tsx
+++ src/app-components/utils.tsx
@@ -46,30 +46,8 @@ export const generateFileData = async (fileObject: {name: string, type: string},
 }
 
 export const getTimeStamp = () => {
-    const dateAndTime = new Date();
-    const dd = ("0" + (dateAndTime.getDate()+1)).slice(-2);
-    const mm = ("0" + (dateAndTime.getMonth()+1)).slice(-2);
-    const date = `${dd}/${mm}/${dateAndTime.getFullYear()}`;
-
-    var hour = dateAndTime.getHours();
-    var hh, amPm;
-    const min = ("0" +dateAndTime.getMinutes()).slice(-2);
-
-    if(hour < 12){
-        hh = hour;
-        amPm="A.M";
-    }else if(hour === 12){
-        hh = hour;
-        (dateAndTime.getMinutes() === 0) ? (amPm="noon"): (amPm="P.M")
-    }else if(hour > 12 && hour !== 24){
-        hh = hour - 12;
-        amPm = "P.M";
-    } else {
-        hh = 12;
-        amPm = "P.M";
-    }
-    ((hh < 10) && (hh = "0" + hh) )
-    const time = `${hh}:${min}${amPm}`;
+    const date = new Date().toLocaleDateString();
+    const time = new Date().toLocaleTimeString();
     const timeStamp = {date: date, time: time};
 
     return timeStamp;
