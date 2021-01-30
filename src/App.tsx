695be9e9359a1242af82b75484328fd5728a74a8
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-01-30 18:56:11 +0530

    Adding Landing Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import Header from './header';
-import { Typography } from 'antd';
 import { useDispatch } from 'react-redux';
 
-import landing_img from './static/assets/landing_img.jpg';
+import Header from './header';
 import Footer from './footer';
 import { getConfigurations } from './store/registrationReducer/actions';
-import PrimaryBtn from './app-components/primaryBtn';
+import LandingPage from './landing-page/index';
 
 import './App.scss';
 import 'antd/dist/antd.css';
-
-const { Title, Paragraph } = Typography;
 
 const App = (props: any) => {
     const { history } = props;
@@ -21,28 +17,14 @@ const App = (props: any) => {
     useEffect(() => {
         dispatch(getConfigurations());
     }, []);
+
     return (
         <div className="app-container">
             <Header history={history} showActions={true} popUpTrigger={{setSignUpPopupVisible, signUpPopupVisible}} />
             <div className="main-content">
-                <img src={landing_img} width="100%" height="90%" />
-                <Typography className="main-content-banner">
-                    <Title className="col-white">Value Proposition</Title>
-                    <Paragraph className="col-white">
-                        In the process of internal desktop applications development, many different
-                        design specs and implementations would be involved, which might cause
-                        designers and developers difficulties and duplication and reduce the
-                        efficiency of development. CI build test
-                    </Paragraph>
-                    <PrimaryBtn
-                        className="vikas-btn-radius wid150"
-                        content="Register"
-                        size="large"
-                        onClick={() => setSignUpPopupVisible(!signUpPopupVisible)}
-                    />
-                </Typography>
-                <Footer />
+                <LandingPage />
             </div>
+            <Footer />
         </div>
     );
 };
