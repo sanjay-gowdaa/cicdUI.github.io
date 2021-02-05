540205a88f325831f6c34ec893fc080df102b46d
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-02-05 11:58:24 +0530

    Adding Home component and fixed-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import { useDispatch } from 'react-redux';
+import { Card, Image, Space, Typography } from 'antd';
 
 import Header from './header';
 import Footer from './footer';
 import { getConfigurations } from './store/registrationReducer/actions';
 import LandingPage from './landing-page/index';
+import Home from './landing-page/home';
+import PrimaryBtn from './app-components/primaryBtn';
+import Banner from './static/assets/banner.png';
+import Join from './static/assets/friends.svg';
 
 import './App.scss';
 import 'antd/dist/antd.css';
+
+const { Title } = Typography;
 
 const App = (props: any) => {
     const { history } = props;
@@ -22,6 +29,21 @@ const App = (props: any) => {
         <div className="app-container">
             <Header history={history} showActions={true} popUpTrigger={{setSignUpPopupVisible, signUpPopupVisible}} />
             <div className="main-content">
+                <Home />
+                <div className="fixed-landing-page-banner">
+                    <Image src={Banner} preview={false} />
+                </div>
+                <div className="fixed-card-join">
+                    <Card className="join-us">
+                        <Space direction="vertical">
+                        <Title className="col-green fixed-title" level={3}>
+                            Be part of VikasBandhu family!
+                        </Title>
+                        <Image src={Join} preview={false} />
+                        <PrimaryBtn className="vikas-btn-radius" content="Register" />
+                        </Space>
+                    </Card>
+                </div>
                 <LandingPage />
             </div>
             <Footer />
