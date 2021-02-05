458d61c68189903d004767b7a4cf98b7d31bda6e
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-02-05 12:08:26 +0530

    Adding Row, Col, and classNames for styling.

--- src/landing-page/ecosystem.tsx
+++ src/landing-page/ecosystem.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Image, Space, Typography } from 'antd';
+import { Col, Image, Row, Space, Typography } from 'antd';
 
 import Tractor from '../static/assets/tractor.svg';
-import Bulletin from '../static/assets/bulletin.svg';
+import Network from '../static/assets/network.svg';
+import Rupee from '../static/assets/rupee.svg';
+import Sync from '../static/assets/sync.svg';
+import Lorry from '../static/assets/lorry.svg';
+import SocialCare from '../static/assets/social-care.svg';
 import Nature from '../static/assets/nature.svg';
 
 const { Text, Title } = Typography;
@@ -10,18 +14,40 @@ const { Text, Title } = Typography;
 const Ecosystem = () => {
     return (
         <div id="ecosystem">
-            <Image src={Tractor} preview={false} />
+            <Image className="tractor-image" src={Tractor} preview={false} />
             <div className="ecosystem-content">
                 <Title className="col-green ecosystem-title" level={2}>At VikasBandhu, we build an ecosystem that would</Title>
-                <Image className="bulletin-image" src={Bulletin} preview={false} />
-                <Space direction="vertical" className="bulletin-content">
-                    <Text className="col-green bulletin-points">Connect buyers and sellers in a simple to use platform.</Text>
-                    <Text className="col-green bulletin-points">Provide dynamic information on pricing.</Text>
-                    <Text className="col-green bulletin-points">Meet demand and supply.</Text>
-                    <Text className="col-green bulletin-points">Facilitate a Door-to-door solution of logistics.</Text>
-                    <Text className="col-green bulletin-points">Boost rural economy by providing local employment.</Text>
-                </Space>
-                <Image className="nature-image" src={Nature} preview={false} />
+                <Row>
+                    <Col span={2}>
+                        <Space direction="vertical" className="bulletin-image" size="large">
+                        <Image className="first-bulletin-image" src={Network} preview={false} />
+                        <Image src={Rupee} preview={false} />
+                        <Image src={Sync} preview={false} />
+                        <Image src={Lorry} preview={false} />
+                        <Image className="last-bulletin-image" src={SocialCare} preview={false} />
+                        </Space>
+                    </Col>
+                    <Col>
+                        <Space direction="vertical" className="bulletin-text" size="small">
+                        <Text className="bulletin-points">
+                            Connect buyers and sellers in a simple to use platform.<br/>
+                        </Text>
+                        <Text className="bulletin-points">
+                            <br/>Provide dynamic information on pricing.<br/>
+                        </Text>
+                        <Text className="bulletin-points">
+                            <br/>Meet demand and supply.<br/>
+                        </Text>
+                        <Text className="bulletin-points">
+                            <br/>Facilitate a Door-to-door solution of logistics.<br/>
+                        </Text>
+                        <Text className="bulletin-points">
+                            <br/>Boost rural economy by providing local employment.
+                        </Text>
+                        </Space>
+                    </Col>
+                </Row>
+                <img className="nature-image" src={Nature} alt="nature-image" />
             </div>
         </div>
     );
