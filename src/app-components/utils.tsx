9eb69c80bc04f1d4d068e522642d46745eedc2ce
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-04-26 21:51:30 +0530

    Uncommenting convertBase64toBlob and exporting

--- src/app-components/utils.tsx
+++ src/app-components/utils.tsx
@@ -1,23 +1,23 @@
-// const converBase64toBlob = (content, contentType) => {
-//     contentType = contentType || '';
-//     var sliceSize = 512;
-//     var byteCharacters = window.atob(content); //method which converts base64 to binary
-//     var byteArrays = [
-//     ];
-//     for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-//       var slice = byteCharacters.slice(offset, offset + sliceSize);
-//       var byteNumbers = new Array(slice.length);
-//       for (var i = 0; i < slice.length; i++) {
-//         byteNumbers[i] = slice.charCodeAt(i);
-//       }
-//       var byteArray = new Uint8Array(byteNumbers);
-//       byteArrays.push(byteArray);
-//     }
-//     var blob = new Blob(byteArrays, {
-//       type: contentType
-//     }); //statement which creates the blob
-//     return blob;
-// }
+export const converBase64toBlob = (content: any, contentType: any) => {
+    contentType = contentType || '';
+    var sliceSize = 512;
+    var byteCharacters = window.atob(content); //method which converts base64 to binary
+    var byteArrays = [
+    ];
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+      var byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+    var blob = new Blob(byteArrays, {
+      type: contentType
+    }); //statement which creates the blob
+    return blob;
+}
 
 
 export const proccessFileToBase64 = (inputFileObj: any) => {
