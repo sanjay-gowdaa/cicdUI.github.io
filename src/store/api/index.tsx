83ca198e1dbd40a6203d26b7282f5da3e248fabd
Shyam <shyam.muniraju@syncron.com>
2021-05-23 13:43:24 +0530

    Shyam : converting to lowercase

--- src/store/api/index.tsx
+++ src/store/api/index.tsx
@@ -31,7 +31,7 @@ const TRANSACTION_API = 'transaction';
 const MATCHES_REJECT_API = `${TRANSACTION_API}/reject`;
 const TRANSACTION_CREATE_API = `${TRANSACTION_API}/create`;
 const TRANSACTION_LIST_API = `${TRANSACTION_API}/user`;
-const USER_COMPLETE_DETAILS = 'getUserCompleteDetails';
+const USER_COMPLETE_DETAILS = 'getusercompletedetails';
 const USER_FILE_API = 'getuserfile';
 const UPDATE_USER_DETAILS = 'updateuserdetails';
 
