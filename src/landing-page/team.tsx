6c3d4911a7789e37c480df45af690f20fb6a4c02
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-11-10 23:11:23 +0530

    Adding Landing page changes.

--- src/landing-page/team.tsx
+++ src/landing-page/team.tsx
@@ -6,8 +6,8 @@ import Nagappa from '../static/assets/nagappa.png';
 import Suma from '../static/assets/suma.png';
 import Sadananda from '../static/assets/sadananda.png';
 import SatishChandra from '../static/assets/SatishChandra.jpg';
-import Sachin from '../static/assets/Sachin.jpg';
-import CVKulkarni from '../static/assets/CVKulkarni.jpg';
+import Ganesh from '../static/assets/Ganesh.png';
+import Shastri from '../static/assets/Shastri.jpeg';
 
 const { Paragraph, Text, Title } = Typography;
 
@@ -30,10 +30,10 @@ const Team = () => {
                                 <Trans
                                     i18nKey="our_team.founders.designation.1"
                                     components={{ italic: <i />, bold: <strong /> }}
-                                />    
+                                />
                             </Title>
                             <Text className="col-orange">{t('our_team.founders.subDesignation.1')}</Text>
-                            <Paragraph style={{textAlign: "justify"}}>
+                            <Paragraph style={{ textAlign: "justify" }}>
                                 <Trans
                                     i18nKey="our_team.founders.description.1"
                                     components={{ italic: <i />, bold: <strong /> }}
@@ -44,7 +44,7 @@ const Team = () => {
                 </Card>
                 <Card>
                     <Space direction="horizontal">
-                        <Space direction="vertical" style={{textAlign: "right"}} size={0}>
+                        <Space direction="vertical" style={{ textAlign: "right" }} size={0}>
                             <Title level={4} className="margin-zero">{t('our_team.founders.name.2')}</Title>
                             <Title level={5} className="margin-zero col-orange">
                                 <Trans
@@ -53,7 +53,7 @@ const Team = () => {
                                 />
                             </Title>
                             <Text className="col-orange">{t('our_team.founders.subDesignation.2')}</Text>
-                            <Paragraph style={{textAlign: "justify"}}>{t('our_team.founders.description.2')}</Paragraph>
+                            <Paragraph style={{ textAlign: "justify" }}>{t('our_team.founders.description.2')}</Paragraph>
                         </Space>
                         <img src={Nagappa} alt="Nagappa" />
                     </Space>
@@ -63,7 +63,7 @@ const Team = () => {
                         <img
                             className="box-shadow"
                             src={Suma}
-                            style={{borderRadius: "15rem", width: "10rem", margin: "2rem"}}
+                            style={{ borderRadius: "15rem", width: "10rem", margin: "2rem" }}
                             alt="Suma"
                         />
                         <Space direction="vertical" size={0}>
@@ -74,28 +74,8 @@ const Team = () => {
                                     components={{ italic: <i />, bold: <strong /> }}
                                 />
                             </Title>
-                            <Paragraph style={{textAlign: "justify"}}>{t('our_team.founders.description.3')}</Paragraph>
+                            <Paragraph style={{ textAlign: "justify" }}>{t('our_team.founders.description.3')}</Paragraph>
                         </Space>
-                    </Space>
-                </Card>
-                <Card>
-                    <Space direction="horizontal">
-                        <Space direction="vertical" style={{textAlign: "right"}} size={0}>
-                            <Title level={4} className="margin-zero">{t('our_team.founders.name.4')}</Title>
-                            <Title level={5} className="col-orange">
-                                <Trans
-                                    i18nKey="our_team.founders.designation.4"
-                                    components={{ italic: <i />, bold: <strong /> }}
-                                />
-                            </Title>
-                            <Paragraph style={{textAlign: "justify"}}>{t('our_team.founders.description.4')}</Paragraph>
-                        </Space>
-                        <img
-                            className="box-shadow"
-                            src={SatishChandra}
-                            style={{width: "10rem", borderRadius: "15rem", margin: "1.5rem"}}
-                            alt="Satish"
-                        />
                     </Space>
                 </Card>
             </Space>
@@ -103,36 +83,53 @@ const Team = () => {
                 <Title className="col-green margin-none team-sub-title" level={3}>{t('our_team.advisors.title')}</Title>
                 <Space direction="vertical">
                     <Card>
-                        <Space direction="horizontal">
-                            <img
-                                src={Sachin}
-                                className="box-shadow"
-                                style={{width: "10rem", borderRadius: "15rem", margin: "1.5rem"}}
-                                alt="Sachin"
-                            />
+                        <Space direction="horizontal" style={{ textAlign: "right" }}>
                             <Space direction="vertical">
                                 <Title level={4} className="margin-zero">{t('our_team.advisors.name.1')}</Title>
                                 <Title level={5} className="margin-zero col-orange">
                                     {t('our_team.advisors.designation.1')}
                                 </Title>
-                                <Paragraph style={{textAlign: "justify"}}>{t('our_team.advisors.description.1')}</Paragraph>
+                                <Paragraph style={{ textAlign: "justify" }}>{t('our_team.advisors.description.1')}</Paragraph>
                             </Space>
+                            <img
+                                src={SatishChandra}
+                                className="box-shadow"
+                                style={{ width: "10rem", borderRadius: "15rem", margin: "1.5rem" }}
+                                alt="SatishChandra"
+                            />
                         </Space>
                     </Card>
                     <Card>
-                        <Space direction="horizontal" style={{textAlign: "right"}}>
+                        <Space direction="horizontal">
+                            <img
+                                src={Ganesh}
+                                className="box-shadow"
+                                style={{ width: "10rem", borderRadius: "15rem", margin: "1.5rem" }}
+                                alt="Ganesh"
+                            />
                             <Space direction="vertical">
                                 <Title level={4} className="margin-zero">{t('our_team.advisors.name.2')}</Title>
                                 <Title level={5} className="margin-zero col-orange">
                                     {t('our_team.advisors.designation.2')}
                                 </Title>
-                                <Paragraph style={{textAlign: "justify"}}>{t('our_team.advisors.description.2')}</Paragraph>
+                                <Paragraph style={{ textAlign: "justify" }}>{t('our_team.advisors.description.2')}</Paragraph>
+                            </Space>
+                        </Space>
+                    </Card>
+                    <Card>
+                        <Space direction="horizontal" style={{ textAlign: "right" }}>
+                            <Space direction="vertical">
+                                <Title level={4} className="margin-zero">{t('our_team.advisors.name.3')}</Title>
+                                <Title level={5} className="margin-zero col-orange">
+                                    {t('our_team.advisors.designation.3')}
+                                </Title>
+                                <Paragraph style={{ textAlign: "justify" }}>{t('our_team.advisors.description.3')}</Paragraph>
                             </Space>
                             <img
-                                src={CVKulkarni}
+                                src={Shastri}
                                 className="box-shadow"
-                                style={{width: "10rem", borderRadius: "15rem", margin: "1.5rem"}}
-                                alt="Kulkarni"
+                                style={{ width: "10rem", borderRadius: "15rem", margin: "1.5rem" }}
+                                alt="Shastri"
                             />
                         </Space>
                     </Card>
