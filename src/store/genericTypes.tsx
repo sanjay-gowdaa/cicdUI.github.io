db02955cb6d3be0bf164f287446105621c358667
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2021-12-11 12:46:36 +0530

    Handling Apmc value when live apmc is not present

--- src/store/genericTypes.tsx
+++ src/store/genericTypes.tsx
@@ -2,32 +2,34 @@ export enum ResponseStatus {
     'SUCCESS' = 'success',
     'ERROR' = 'error',
     'FAILED' = 'failed'
-}
+};
 
 export enum ResponseCode {
     'SUCCESS' = 200,
     'ERROR' = 400,
-}
+};
+
 export enum UserTypes {
     SELLER = 'Seller',
     BUYER = 'Buyer'
-}
+};
 
 export type LiveApmcRates = {
     region: string;
     commodity: string;
     variety: string;
-}
+};
 
 export type UpdatedLiveApmcRatesQuery = {
     grade: string;
-    item_name: string;
+    produce: string;
     variety: string;
     district: string;
     category: string;
-}
+};
 
 export type ApmcApiResponseBase = {
+    is_actual: boolean;
     latest_apmc_price: number;
     previousLatestApmcPrice: number;
-} 
+};
