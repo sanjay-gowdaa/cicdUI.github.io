f19ad9662e1181ba769a365bb5314b370b0b1d2d
ShilpaSamparkbindhu <shilpa@samparkbindhu.in>
2022-01-07 11:24:10 +0530

    Correcting indentation.

--- src/landing-page/home.tsx
+++ src/landing-page/home.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Alert, Button, Carousel, Image, Typography } from 'antd';
 import { Trans, useTranslation } from 'react-i18next';
-import Marquee from "react-fast-marquee";
+import Marquee from 'react-fast-marquee';
 
 import Logo from '../static/assets/vbLogo.png';
 import KannadaLogo from '../static/assets/kannadaLogo.png'
 import VBOne from '../static/assets/Vikasbandhu_One.jpg';
-//import VBTwo from '../static/assets/Vikasbandhu_Two.jPG';
 import VBTwo from '../static/assets/Vb_launch.jpg'
 import VBThree from '../static/assets/Vikasbandhu_Three.jpg';
 import VBFour from '../static/assets/Vikasbandhu_Four.jpg';
@@ -17,18 +16,18 @@ const { Text, Title } = Typography;
 
 const Home = () => {
     const { t } = useTranslation('common');
-    const customStyles = isEnglish(t("language")) ? englishStyling : kannadaStyling;
-    const logo =  isEnglish(t("language")) ? Logo : KannadaLogo;
+    const customStyles = isEnglish(t('language')) ? englishStyling : kannadaStyling;
+    const logo = isEnglish(t('language')) ? Logo : KannadaLogo;
 
     const goToAim = (url: any) => {
         window.location = url;
     };
 
     return (
-        <div id="home">
-            <div className="home-content-wrapper">
+        <div id='home'>
+            <div className='home-content-wrapper'>
                 <Alert
-                    className="animate-alert"
+                    className='animate-alert'
                     message={
                         <Marquee
                             pauseOnHover={true}
@@ -36,67 +35,67 @@ const Home = () => {
                             gradient={false}
                         >
                             <Trans>
-                            <b>&nbsp; &nbsp;&nbsp; &nbsp;</b>
-                            <Trans
-                                i18nKey="home_page.flash_text"
-                                components={{ italic: <i />, bold: <strong />}}
-                            />
-                            <b>&nbsp; &nbsp;{t('home_page.update_1')}&nbsp; &nbsp;</b>
-                            <Trans
-                                i18nKey="home_page.update_2"
-                                components={{ italic: <i />, bold: <strong /> }}
-                            />
-                            <b>&nbsp; &nbsp;&nbsp;</b>
-                            <Trans
-                                i18nKey="home_page.update_3"
-                                components={{ italic: <i />, bold: <strong /> }}
-                            />
+                                <b>&nbsp; &nbsp;&nbsp; &nbsp;</b>
+                                <Trans
+                                    i18nKey='home_page.flash_text'
+                                    components={{ italic: <i />, bold: <strong /> }}
+                                />
+                                <b>&nbsp; &nbsp;{t('home_page.update_1')}&nbsp; &nbsp;</b>
+                                <Trans
+                                    i18nKey='home_page.update_2'
+                                    components={{ italic: <i />, bold: <strong /> }}
+                                />
+                                <b>&nbsp; &nbsp;&nbsp;</b>
+                                <Trans
+                                    i18nKey='home_page.update_3'
+                                    components={{ italic: <i />, bold: <strong /> }}
+                                />
                             </Trans>
                         </Marquee>
                     }
                     banner
                 />
-                <Carousel className="home-carousel" autoplay autoplaySpeed={5000}>
-                    <Image height={"87vh"} width={"100%"} src={VBOne} preview={false} />
-                    <Image height={"87vh"} width={"100%"} src={VBTwo} preview={false} />
-                    <Image height={"87vh"} width={"100%"} src={VBThree} preview={false} />
-                    <Image height={"87vh"} width={"100%"} src={VBFour} preview={false} />
-                    <Image height={"87vh"} width={"100%"} src={VBFive} preview={false} />
+                <Carousel className='home-carousel' autoplay autoplaySpeed={5000}>
+                    <Image height={'87vh'} width={'100%'} src={VBOne} preview={false} />
+                    <Image height={'87vh'} width={'100%'} src={VBTwo} preview={false} />
+                    <Image height={'87vh'} width={'100%'} src={VBThree} preview={false} />
+                    <Image height={'87vh'} width={'100%'} src={VBFour} preview={false} />
+                    <Image height={'87vh'} width={'100%'} src={VBFive} preview={false} />
                 </Carousel>
-                <div className="mobile-home-content">
-                    <Title className="col-white home-title" level={1}>
+                <div className='mobile-home-content'>
+                    <Title className='col-white home-title' level={1}>
                         {t('title')}
                     </Title>
-                    <Text className="col-white home-text">{t('home_page.mobile_home_text')}</Text>
-                    <br/>
-                    <Button className="explore-vb-btn" onClick={() => goToAim("#aim")}>
-                        <Title className="col-white margin-none" level={5}>
-                           {t('landing_page.actions.explore')}
+                    <Text className='col-white home-text'>{t('home_page.mobile_home_text')}</Text>
+                    <br />
+                    <Button className='explore-vb-btn' onClick={() => goToAim('#aim')}>
+                        <Title className='col-white margin-none' level={5}>
+                            {t('landing_page.actions.explore')}
                         </Title>
                     </Button>
                 </div>
                 <div className={customStyles.homeContent}>
-                    <Image className="logo" height={100} width={100} src={logo} preview={false} />
-                    <div className="home-paragraph">
-                        <Title className="col-white" level={4}>
+                    <Image className='logo' height={100} width={100} src={logo} preview={false} />
+                    <div className='home-paragraph'>
+                        <Title className='col-white' level={4}>
                             {t('title')} -
                         </Title>
-                        <Title className="col-white margin-none" level={5}>
+                        <Title className='col-white margin-none' level={5}>
                             {t('home_page.text_1')}
                         </Title>
-                        <Title className="col-white margin-none" level={5}>
+                        <Title className='col-white margin-none' level={5}>
                             {t('home_page.text_2')}
                         </Title>
-                        <Title className="col-white margin-none" level={5}>
+                        <Title className='col-white margin-none' level={5}>
                             {t('home_page.text_3')}
                         </Title>
-                        <Title className="col-white margin-none" level={5}>
+                        <Title className='col-white margin-none' level={5}>
                             {t('home_page.text_4')}
                         </Title>
                     </div>
-                    <Button className="explore-vb-btn" onClick={() => goToAim("#aim")}>
-                        <Title className="col-white margin-none" level={5}>
-                           {t('landing_page.actions.explore')}
+                    <Button className='explore-vb-btn' onClick={() => goToAim('#aim')}>
+                        <Title className='col-white margin-none' level={5}>
+                            {t('landing_page.actions.explore')}
                         </Title>
                     </Button>
                 </div>
--- src/landing-page/legal.tsx
+++ src/landing-page/legal.tsx
@@ -14,87 +14,87 @@ const Legal = () => {
     const { t } = useTranslation('common');
 
     return (
-        <div id="legal">
-            <Title className="col-white legal-title" level={2}>
+        <div id='legal'>
+            <Title className='col-white legal-title' level={2}>
                 {t('legal_page.title')}
             </Title>
-            <Image className="accept-image" src={Accept} preview={false} />
-            <Tabs className="legal-tab" defaultActiveKey="1" type="card">
+            <Image className='accept-image' src={Accept} preview={false} />
+            <Tabs className='legal-tab' defaultActiveKey='1' type='card'>
                 <TabPane
-                    className="tnc-tab"
+                    className='tnc-tab'
                     tab={t('landing_page.actions.terms_and_conditions')}
-                    key="1"
+                    key='1'
                 >
                     <Paragraph>
                         <ul>
                             <li>
-                                Vikasbandhu is the trademark owned by SamparkBindhu Solutions 
-                                Private Limited("<b>SamparkBindhu</b>"). All back-end support 
-                                for the application has been created and managed by SamparkBindhu 
-                                Solutions Private Limited and Vikasbandhu is the user interface 
-                                ("aslo hearin after referred as <b>Platform</b>").
+                                Vikasbandhu is the trademark owned by SamparkBindhu Solutions
+                                Private Limited('<b>SamparkBindhu</b>'). All back-end support
+                                for the application has been created and managed by SamparkBindhu
+                                Solutions Private Limited and Vikasbandhu is the user interface
+                                ('aslo hearin after referred as <b>Platform</b>').
                             </li>
                             <li>
-                                Platform is only a facilitator between the users who are selling 
+                                Platform is only a facilitator between the users who are selling
                                 agriculture produce crops grown on agricultural land and (<b>Seller[s]</b>)
                                 and users interested in purchasing such agriculture produce (<b>Buyer[s]</b>),
-                                to help strike a mutually agreeable deal and has no role in trading or 
-                                warehousing and is not the authority on the quality of the agriculture 
+                                to help strike a mutually agreeable deal and has no role in trading or
+                                warehousing and is not the authority on the quality of the agriculture
                                 produce available on the platform.
                             </li>
                             <li>
-                                By accessing and registering with the platform, the users confirm that the 
-                                Users are in agreement with and bound by the terms of service contained in 
+                                By accessing and registering with the platform, the users confirm that the
+                                Users are in agreement with and bound by the terms of service contained in
                                 the Terms and Conditions outlined below. These terms apply to the entire website,
-                                mobile application, and any mail exchange that may happen during course of 
+                                mobile application, and any mail exchange that may happen during course of
                                 business between the Users and the Platform.
                             </li>
                             <li>
                                 By using the Platform, the Users are accepting these terms and conditions in full
-                                and shall be legally bound to said terms and conditions, so the User should read 
-                                them carefully. Access to and use of Platform depends soely on the acceptance 
+                                and shall be legally bound to said terms and conditions, so the User should read
+                                them carefully. Access to and use of Platform depends soely on the acceptance
                                 of the Terms and Conditions.
                             </li>
                         </ul>
                     </Paragraph>
-                    <Button className="read-more-button vikas-btn-radius">
-                        <Link to={terms} target="_blank">{t('landing_page.actions.read_more')}</Link>
+                    <Button className='read-more-button vikas-btn-radius'>
+                        <Link to={terms} target='_blank'>{t('landing_page.actions.read_more')}</Link>
                     </Button>
                 </TabPane>
                 <TabPane
-                    className="privacy-tab"
+                    className='privacy-tab'
                     tab={t('landing_page.actions.privacy_policy')}
-                    key="2"
+                    key='2'
                 >
                     <Paragraph>
                         <ul>
                             <li>
-                                SamparkBindhu Solutions Private Limited (“<b>SamparkBindhu</b>/<b>Company</b>”) is a company 
-                                registered under the Companies Act, 2013, bearing CIN U72900KA2020PTC137512 and having its 
-                                registered office at #617, "Shivam", Royal Park Residency, J P Nagar 9th Phase, Bengaluru 
+                                SamparkBindhu Solutions Private Limited (“<b>SamparkBindhu</b>/<b>Company</b>”) is a company
+                                registered under the Companies Act, 2013, bearing CIN U72900KA2020PTC137512 and having its
+                                registered office at #617, 'Shivam', Royal Park Residency, J P Nagar 9th Phase, Bengaluru
                                 Bangalore, Karnataka - 560062, India
                             </li>
                             <li>
-                                This Privacy Policy (“<b>Policy</b>”) sets forth the reasonable security practices and procedures 
-                                adopted by SamparkBindhu and shall apply to the use and disclosure and sharing of sensitive 
-                                personal data or information (“<b>Personal Identifiable Information</b>” or “<b>PII</b>”) provided by the 
-                                registered users of the website <b>[●]</b> (“<b>Website</b>”), which is owned and operated by SamparkBindhu. 
-                                This policy is equally applicable on the visitors to the Website. The Policy outlines the 
-                                various measures undertaken by SamparkBindhu to provide Users with a convenient and safe 
-                                online/ experience, while  maintaining and safeguarding the  confidentiality, integrity, 
-                                and security of Users PII, during storage and transmission through the World Wide Web. This 
-                                Policy explains how SamparkBindhu protects PII provided by Users through the Website and how 
+                                This Privacy Policy (“<b>Policy</b>”) sets forth the reasonable security practices and procedures
+                                adopted by SamparkBindhu and shall apply to the use and disclosure and sharing of sensitive
+                                personal data or information (“<b>Personal Identifiable Information</b>” or “<b>PII</b>”) provided by the
+                                registered users of the website <b>[●]</b> (“<b>Website</b>”), which is owned and operated by SamparkBindhu.
+                                This policy is equally applicable on the visitors to the Website. The Policy outlines the
+                                various measures undertaken by SamparkBindhu to provide Users with a convenient and safe
+                                online/ experience, while  maintaining and safeguarding the  confidentiality, integrity,
+                                and security of Users PII, during storage and transmission through the World Wide Web. This
+                                Policy explains how SamparkBindhu protects PII provided by Users through the Website and how
                                 SamparkBindhu store and use that information, to deliver the services on the Website.
                             </li>
                             <li>
-                                Please read this Policy carefully, it is subject to change at any time and is advised that Users 
-                                review this Policy periodically. Any change to this Policy will become effective as soon as it is 
+                                Please read this Policy carefully, it is subject to change at any time and is advised that Users
+                                review this Policy periodically. Any change to this Policy will become effective as soon as it is
                                 posted or linked to the revised Policy on the Website.
                             </li>
                         </ul>
                     </Paragraph>
-                    <Button className="read-more-button vikas-btn-radius">
-                        <Link to={terms} target="_blank">{t('landing_page.actions.read_more')}</Link>
+                    <Button className='read-more-button vikas-btn-radius'>
+                        <Link to={terms} target='_blank'>{t('landing_page.actions.read_more')}</Link>
                     </Button>
                 </TabPane>
             </Tabs>
