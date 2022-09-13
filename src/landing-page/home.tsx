8f069fbbf1c062de675876e7e0428b8e399d2b7f
sudeshvs <sudesh@samparkbindhu.in>
2022-09-13 14:44:26 +0530

    recent-mobile responsiveness

--- src/landing-page/home.tsx
+++ src/landing-page/home.tsx
@@ -13,67 +13,102 @@ import { LandingDivider } from '../app-components/landingDivider';
 
 const { Title } = Typography;
 
-const Home = ({ history, popUpTrigger, setSignUpPopupVisible }: { history: History, popUpTrigger: any, setSignUpPopupVisible: Function }) => {
+const Home = ({
+    history,
+    popUpTrigger,
+    setSignUpPopupVisible,
+}: {
+    history: History;
+    popUpTrigger: any;
+    setSignUpPopupVisible: Function;
+}) => {
     const { signUpPopupVisible } = popUpTrigger;
     const { t } = useTranslation('common');
 
     return (
-        <div id='home'>
-            <div className='home-wrapper'>
-            <Row>
-                <Col span={10} xs={24} md={10}>
-                    <Title level={2} style={{width:'500px',fontStyle: 'normal',
-fontWeight: 600,fontSize: '48px'}}>Sell.&nbsp;Buy.&nbsp;AgriProduce.</Title>
-                    <Title level={5}>{t('home_page.text1')}</Title>
-                    <div className='wimage'></div>
-                    <div className='registerComponent'>
-                        <p>{t('home_page.text2')}</p>
-                        <Button onClick={() => setSignUpPopupVisible(!signUpPopupVisible)}>Register</Button>
-                        <Modal
-                            title={null}
-                            visible={signUpPopupVisible}
-                            footer={null}
-                            maskClosable={false}
-                            className='custom-register-modal'
-                            onCancel={() => setSignUpPopupVisible(!signUpPopupVisible)}
-                            centered
-                            wrapClassName='register-popup-container'
+        <div id="home">
+            <div className="home-wrapper">
+                <Row>
+                    <Col span={10} xs={24} md={10}>
+                        <Title
+                            level={2}
+                            className = 'homeTitle'
                         >
-                            <Register history={history} setSignUpPopupVisible={setSignUpPopupVisible} />
-                        </Modal>
-                    </div>
-                    <div className='googlePlay'>
-                        <Button size='small' type ='link' href='https://play.google.com/store/apps/details?id=com.vikasBandhu' target='_blank'><img src={googlePlay}/></Button>
-                    </div>
-                    <div className='headFooter'>
-                        <div className='firstL'>
-                            <p>{t('home_page.text3')}</p>
-                            <a href='#updates'>{t('home_page.text4')}</a>
+                            Sell.&nbsp;Buy.&nbsp;AgriProduce.
+                        </Title>
+                        <Title level={5}>{t('home_page.text1')}</Title>
+                        <div className="wimage"></div>
+                        <div className="registerComponent">
+                            <p>{t('home_page.text2')}</p>
+                            <Button onClick={() => setSignUpPopupVisible(!signUpPopupVisible)}>
+                                Register
+                            </Button>
+                            <Modal
+                                title={null}
+                                visible={signUpPopupVisible}
+                                footer={null}
+                                maskClosable={false}
+                                className="custom-register-modal"
+                                onCancel={() => setSignUpPopupVisible(!signUpPopupVisible)}
+                                centered
+                                wrapClassName="register-popup-container"
+                            >
+                                <Register
+                                    history={history}
+                                    setSignUpPopupVisible={setSignUpPopupVisible}
+                                />
+                            </Modal>
                         </div>
-                        <div className='secondL'>
-                            <Breadcrumb separator='|'>
-                                <Breadcrumb.Item href='#mvp'>{t('home_page.text5')}</Breadcrumb.Item>
-                                <Breadcrumb.Item href='#foodExb'>{t('home_page.text6')}</Breadcrumb.Item>
-                                <Breadcrumb.Item href='#academia'>{t('home_page.text7')}</Breadcrumb.Item>
-                            </Breadcrumb>
+                        <div className="googlePlay">
+                            <Button
+                                size="small"
+                                type="link"
+                                href="https://play.google.com/store/apps/details?id=com.vikasBandhu"
+                                target="_blank"
+                            >
+                                <img src={googlePlay} />
+                            </Button>
                         </div>
-                    </div>
-                </Col>
-                <Col span={14} xs={0} md={10}>
-                    <div className='courselCircle'>
-                        <Carousel autoplay speed={2500} dots={false} effect='fade' className='courselUnit'>
-                            <img src={firstImage} className='contentStyle' alt='firstImage' />
-                            <img src={secondImage} className='contentStyle' alt='secondImage' />
-                            <img src={thirdImage} className='contentStyle' alt='thirdImage'/>
-                            <img src={fourthImage} className='contentStyle' alt='fourthImage'/>
-                            <img src={fifthImage} className='contentStyle' alt='fifthImage'/>
-                        </Carousel>
-                    </div>
-                </Col>
-            </Row>
-            
-        </div>
-        <LandingDivider className='pageDivider'/>
+                        <div className="headFooter">
+                            <div className="firstL">
+                                <p>{t('home_page.text3')}</p>
+                                <a href="#updates">{t('home_page.text4')}</a>
+                            </div>
+                            <div className="secondL">
+                                <Breadcrumb separator="|">
+                                    <Breadcrumb.Item href="#mvp">
+                                        {t('home_page.text5')}
+                                    </Breadcrumb.Item>
+                                    <Breadcrumb.Item href="#foodExb">
+                                        {t('home_page.text6')}
+                                    </Breadcrumb.Item>
+                                    <Breadcrumb.Item href="#academia">
+                                        {t('home_page.text7')}
+                                    </Breadcrumb.Item>
+                                </Breadcrumb>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col span={14} xs={0} md={10}>
+                        <div className="courselCircle">
+                            <Carousel
+                                autoplay
+                                speed={2500}
+                                dots={false}
+                                effect="fade"
+                                className="courselUnit"
+                            >
+                                <img src={firstImage} className="contentStyle" alt="firstImage" />
+                                <img src={secondImage} className="contentStyle" alt="secondImage" />
+                                <img src={thirdImage} className="contentStyle" alt="thirdImage" />
+                                <img src={fourthImage} className="contentStyle" alt="fourthImage" />
+                                <img src={fifthImage} className="contentStyle" alt="fifthImage" />
+                            </Carousel>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <LandingDivider className="pageDivider" />
         </div>
     );
 };
