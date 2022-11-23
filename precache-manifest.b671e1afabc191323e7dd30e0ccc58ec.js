a40c877bec7301e9882db33f9fd9c185ec92df66
suprr <sanjay121260@gmail.com>
2022-11-23 13:40:08 +0530

    Updates

---
+++ precache-manifest.b671e1afabc191323e7dd30e0ccc58ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8744f6c3334c875fc0e",
+    "url": "/cicdUI.github.io/static/css/main.30a0cb9e.chunk.css"
+  },
+  {
+    "revision": "a8744f6c3334c875fc0e",
+    "url": "/cicdUI.github.io/static/js/main.a02b8f34.chunk.js"
+  },
+  {
+    "revision": "9b8b48d61c13f74b93f6",
+    "url": "/cicdUI.github.io/static/js/runtime~main.3ca20a3e.js"
+  },
+  {
+    "revision": "364c1d17f1cc669e2a04",
+    "url": "/cicdUI.github.io/static/css/2.b8ceef78.chunk.css"
+  },
+  {
+    "revision": "364c1d17f1cc669e2a04",
+    "url": "/cicdUI.github.io/static/js/2.03ded776.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cicdUI.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cicdUI.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cicdUI.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cicdUI.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cicdUI.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "72cbe95ff9bc5b74273d6263ee1b7316",
+    "url": "/cicdUI.github.io/index.html"
+  }
+];
